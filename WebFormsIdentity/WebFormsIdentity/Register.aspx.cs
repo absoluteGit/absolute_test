47ecf0ece2d7f600a841726dc21fe206a8671667
absoluteGit <elobos@absolute.com>
2016-02-10 14:40:22 -0800

    Project now allows for basic registration, login, logout, and claims collection generation.

--- WebFormsIdentity/WebFormsIdentity/Register.aspx.cs
+++ WebFormsIdentity/WebFormsIdentity/Register.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
 using System.Linq;
+using System.Web;
 
 namespace WebFormsIdentity
 {
@@ -12,12 +14,16 @@ namespace WebFormsIdentity
             // Default UserStore constructor uses the default connection string named: DefaultConnection
             var userStore = new UserStore<IdentityUser>();
             var manager = new UserManager<IdentityUser>(userStore);
-
             var user = new IdentityUser() { UserName = UserName.Text };
+
             IdentityResult result = manager.Create(user, Password.Text);
 
             if (result.Succeeded)
             {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+                Response.Redirect("~/Login.aspx");
                 StatusMessage.Text = string.Format("User {0} was created successfully!", user.UserName);
             }
             else
